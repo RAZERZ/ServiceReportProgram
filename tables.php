fe217f73e46b72ea6bedf622574e76db81d0de52
Rami Abou Zahra <ramiabouzahra@gmail.com>
2017-12-02 22:14:31 +0100

    Sigh... Added support for Windows

--- tables.php
+++ tables.php
@@ -239,44 +239,44 @@ $ccValueOther = $row['ccValueOther'];
 
 <td>
     <form action="print.php" method="post">
-        <input type="hidden" name="serviceNummer" value='<? echo "$serviceNummer"; ?>'>
-        <input type="hidden" name="repairName" value='<? echo "$repairName"; ?>'>
-        <input type="hidden" name="kundensNamn" value='<? echo "$kundensNamn"; ?>'>
-        <input type="hidden" name="kundensAdress" value='<? echo "$kundensAdress"; ?>'>
-        <input type="hidden" name="kundensMail" value='<? echo "$kundensMail"; ?>'>
-        <input type="hidden" name="kundensNummer" value='<? echo "$kundensNummer"; ?>'>
+        <input type="hidden" name="serviceNummer" value='<?php echo "$serviceNummer"; ?>'>
+        <input type="hidden" name="repairName" value='<?php echo "$repairName"; ?>'>
+        <input type="hidden" name="kundensNamn" value='<?php echo "$kundensNamn"; ?>'>
+        <input type="hidden" name="kundensAdress" value='<?php echo "$kundensAdress"; ?>'>
+        <input type="hidden" name="kundensMail" value='<?php echo "$kundensMail"; ?>'>
+        <input type="hidden" name="kundensNummer" value='<?php echo "$kundensNummer"; ?>'>
 
-        <input type="hidden" name="daCheckMobo" value='<? echo "$daCheckMobo"; ?>'>
-        <input type="hidden" name="daErrMobo" value='<? echo "$daErrMobo"; ?>'>
-        <input type="hidden" name="daCheckGpu" value='<? echo "$daCheckGpu"; ?>'>
-        <input type="hidden" name="daErrGpu" value='<? echo "$daErrGpu"; ?>'>
-        <input type="hidden" name="daCheckCpu" value='<? echo "$daCheckCpu"; ?>'>
-        <input type="hidden" name="daErrCpu" value='<? echo "$daErrCpu"; ?>'>
-        <input type="hidden" name="daCheckPsu" value='<? echo "$daCheckPsu"; ?>'>
-        <input type="hidden" name="daErrPsu" value='<? echo "$daErrPsu"; ?>'>
-        <input type="hidden" name="daCheckHdd" value='<? echo "$daCheckHdd"; ?>'>
-        <input type="hidden" name="daErrHdd" value='<? echo "$daErrHdd"; ?>'>
-        <input type="hidden" name="daCheckCool" value='<? echo "$daCheckCool"; ?>'>
-        <input type="hidden" name="daErrCool" value='<? echo "$daErrCool"; ?>'>
-        <input type="hidden" name="daCheckOther" value='<? echo "$daCheckOther"; ?>'>
-        <input type="hidden" name="daOtherValue" value='<? echo "$daOtherValue"; ?>'>
-        <input type="hidden" name="daErrOther" value='<? echo "$daErrOther"; ?>'>
+        <input type="hidden" name="daCheckMobo" value='<?php echo "$daCheckMobo"; ?>'>
+        <input type="hidden" name="daErrMobo" value='<?php echo "$daErrMobo"; ?>'>
+        <input type="hidden" name="daCheckGpu" value='<?php echo "$daCheckGpu"; ?>'>
+        <input type="hidden" name="daErrGpu" value='<?php echo "$daErrGpu"; ?>'>
+        <input type="hidden" name="daCheckCpu" value='<?php echo "$daCheckCpu"; ?>'>
+        <input type="hidden" name="daErrCpu" value='<?php echo "$daErrCpu"; ?>'>
+        <input type="hidden" name="daCheckPsu" value='<?php echo "$daCheckPsu"; ?>'>
+        <input type="hidden" name="daErrPsu" value='<?php echo "$daErrPsu"; ?>'>
+        <input type="hidden" name="daCheckHdd" value='<?php echo "$daCheckHdd"; ?>'>
+        <input type="hidden" name="daErrHdd" value='<?php echo "$daErrHdd"; ?>'>
+        <input type="hidden" name="daCheckCool" value='<?php echo "$daCheckCool"; ?>'>
+        <input type="hidden" name="daErrCool" value='<?php echo "$daErrCool"; ?>'>
+        <input type="hidden" name="daCheckOther" value='<?php echo "$daCheckOther"; ?>'>
+        <input type="hidden" name="daOtherValue" value='<?php echo "$daOtherValue"; ?>'>
+        <input type="hidden" name="daErrOther" value='<?php echo "$daErrOther"; ?>'>
 
-        <input type="hidden" name="ccCheckSearch" value='<? echo "$ccCheckSearch"; ?>'>
-        <input type="hidden" name="ccCheckMobo" value='<? echo "$ccCheckMobo"; ?>'>
-        <input type="hidden" name="ccPriceMobo" value='<? echo "$ccPriceMobo"; ?>'>
-        <input type="hidden" name="ccCheckGpu" value='<? echo "$ccCheckGpu"; ?>'>
-        <input type="hidden" name="ccPriceGpu" value='<? echo "$ccPriceGpu"; ?>'>
-        <input type="hidden" name="ccCheckCpu" value='<? echo "$ccCheckCpu"; ?>'>
-        <input type="hidden" name="ccPriceCpu" value='<? echo "$ccPriceCpu"; ?>'>
-        <input type="hidden" name="ccCheckPsu" value='<? echo "$ccCheckPsu"; ?>'>
-        <input type="hidden" name="ccPricePsu" value='<? echo "$ccPricePsu"; ?>'>
-        <input type="hidden" name="ccCheckHdd" value='<? echo "$ccCheckHdd"; ?>'>
-        <input type="hidden" name="ccPriceHdd" value='<? echo "$ccPriceHdd"; ?>'>
-        <input type="hidden" name="ccCheckCool" value='<? echo "$ccCheckCool"; ?>'>
-        <input type="hidden" name="ccPriceCool" value='<? echo "$ccPriceCool"; ?>'>
-        <input type="hidden" name="ccCheckOther" value='<? echo "$ccCheckOther"; ?>'>
-        <input type="hidden" name="ccValueOther" value='<? echo "$ccValueOther"; ?>'>
+        <input type="hidden" name="ccCheckSearch" value='<?php echo "$ccCheckSearch"; ?>'>
+        <input type="hidden" name="ccCheckMobo" value='<?php echo "$ccCheckMobo"; ?>'>
+        <input type="hidden" name="ccPriceMobo" value='<?php echo "$ccPriceMobo"; ?>'>
+        <input type="hidden" name="ccCheckGpu" value='<?php echo "$ccCheckGpu"; ?>'>
+        <input type="hidden" name="ccPriceGpu" value='<?php echo "$ccPriceGpu"; ?>'>
+        <input type="hidden" name="ccCheckCpu" value='<?php echo "$ccCheckCpu"; ?>'>
+        <input type="hidden" name="ccPriceCpu" value='<?php echo "$ccPriceCpu"; ?>'>
+        <input type="hidden" name="ccCheckPsu" value='<?php echo "$ccCheckPsu"; ?>'>
+        <input type="hidden" name="ccPricePsu" value='<?php echo "$ccPricePsu"; ?>'>
+        <input type="hidden" name="ccCheckHdd" value='<?php echo "$ccCheckHdd"; ?>'>
+        <input type="hidden" name="ccPriceHdd" value='<?php echo "$ccPriceHdd"; ?>'>
+        <input type="hidden" name="ccCheckCool" value='<?php echo "$ccCheckCool"; ?>'>
+        <input type="hidden" name="ccPriceCool" value='<?php echo "$ccPriceCool"; ?>'>
+        <input type="hidden" name="ccCheckOther" value='<?php echo "$ccCheckOther"; ?>'>
+        <input type="hidden" name="ccValueOther" value='<?php echo "$ccValueOther"; ?>'>
 
         <input type="submit" value="Skriv Ut">
     </form>
